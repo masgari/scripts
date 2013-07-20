00784d917a4f4482461ab44b4d3d1a2f02fb142a
masgari <mammad.asgari@gmail.com>
2013-07-20 17:00:03 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,11 +1,12 @@
 O=/etc/bash.bashrc
 sudo sed -i "$ a\export HISTFILESIZE=10000" $O
 sudo sed -i "$ a\HISTCONTROL=ignoredups" $O
-sudo sed -i "$ a\alias trim="sed -E 's/\s*$//g' | sed -E 's/^\s*//g'"" $O
+sudo sed -i "$ a\alias trim='sed -E 's/\s*$//g' | sed -E 's/^\s*//g''" $O
 sudo sed -i "$ a\alias fgrep='fgrep --colour'" $O
 sudo sed -i "$ a\alias egrep='egrep --colour'" $O
 sudo sed -i "$ a\alias grep='grep --colour'" $O
 sudo sed -i "$ a\alias u='cd ..'" $O
+sudo sed -i "$ a\alias b='cd -'" $O
 sudo sed -i "$ a\alias l='ls -ahl --color'" $O
 sudo sed -i "$ a\alias du1='du -h --max-depth=1'" $O
 sudo sed -i "$ a\alias sudo='sudo '" $O 
