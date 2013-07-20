edda1bad27458e782ab710ca216627000dbc06f4
masgari <mammad.asgari@gmail.com>
2013-07-20 16:34:54 +0100

    Update get-all.sh

--- get-all.sh
+++ get-all.sh
@@ -1,6 +1,8 @@
-B=https://raw.github.com/masgari/scripts/master/
+B=https://raw.github.com/masgari/scripts/master
 
 wget $B/aliases.sh
 wget $B/sudo-no-pw.sh
 wget $B/static-ip.sh
 wget $B/samba.sh
+
+chmod +x *.sh
