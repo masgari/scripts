840af5b6c33cb24ce57edd694ee262893c7be8df
masgari <mammad.asgari@gmail.com>
2013-08-12 09:23:17 +0100

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -1,10 +1,9 @@
-
 #setup samba shares
 O=/etc/samba/smb.conf
 D=/media/share
 M="[share]\n\tcomment = Public share\n\tpath = $D\n\tbrowseable = yes\n\tread only = no\n\tguest ok = yes\n\tcreate mask = 0777\n"
 
-sudo sudo sed -i "$ a$M" $O
+sudo sed -i "$ a$M" $O
 sudo mkdir $D
 sudo chmod 777 $D
 sudo service smbd restart
