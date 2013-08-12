0e6474deb9a6f1970a8c20e4a36ed6ef5d6082f7
masgari <mammad.asgari@gmail.com>
2013-08-12 09:31:02 +0100

    Update open-files.sh

--- open-files.sh
+++ open-files.sh
@@ -1,2 +1,9 @@
 LIMITS="/etc/security/limits.conf"
 U=$(id -u -n)
+N=100000
+LINES="$U\tsoft\tnofile\t$N\n$U\thard\tnofile\t$N\n"
+sudo sed -i "$ a$LINES" $LIMITS
+
+PAM="/etc/pam.d/common-session"
+PL="session\trequired\tpam_limits.so"
+sudo sed -i "$ a$PL" $PAM
