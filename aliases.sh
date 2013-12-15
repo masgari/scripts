f765f330f21b92d05ddc62036006cacf17634fe9
masgari <mammad.asgari@gmail.com>
2013-12-15 17:42:40 +0000

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,16 +1,17 @@
 O=/etc/bash.bashrc
-sudo sed -i "$ a\export HISTFILESIZE=10000" $O
-sudo sed -i "$ a\HISTCONTROL=ignoredups" $O
-sudo sed -i "$ a\alias fgrep='fgrep --colour'" $O
-sudo sed -i "$ a\alias egrep='egrep --colour'" $O
-sudo sed -i "$ a\alias grep='grep --colour'" $O
-sudo sed -i "$ a\alias u='cd ..'" $O
-sudo sed -i "$ a\alias b='cd -'" $O
-sudo sed -i "$ a\alias l='ls -ahl --color'" $O
-sudo sed -i "$ a\alias du1='du -h --max-depth=1'" $O
-sudo sed -i "$ a\alias sudo='sudo '" $O 
+C=sudo sed -i
+$C "$ a\export HISTFILESIZE=10000" $O
+$C "$ a\HISTCONTROL=ignoredups" $O
+$C "$ a\alias fgrep='fgrep --colour'" $O
+$C "$ a\alias egrep='egrep --colour'" $O
+$C "$ a\alias grep='grep --colour'" $O
+$C "$ a\alias u='cd ..'" $O
+$C "$ a\alias b='cd -'" $O
+$C "$ a\alias l='ls -ahl --color'" $O
+$C "$ a\alias du1='du -h --max-depth=1'" $O
+$C "$ a\alias sudo='sudo '" $O 
 #sudo sed -i "$ a\alias rmx='rm `ls | grep -v $1`'"
 #sudo sed -i "$ a\alias trim='sed -E 's/\s*$//g' | sed -E 's/^\s*//g''" $O
-sudo sed -i "$ a\alias http-server='python -m SimpleHTTPServer'" $O
-sudo sed -i "$ a\alias portwho='netstat -tulpn | grep $1'" $O
+$C "$ a\alias http-server='python -m SimpleHTTPServer'" $O
+$C "$ a\alias portwho='netstat -tulpn | grep $1'" $O
 source $O
