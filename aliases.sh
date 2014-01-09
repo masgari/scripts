acdcfbd25534d78875482d2389f64f6549b8da0b
masgari <mammad.asgari@gmail.com>
2014-01-09 12:25:59 +0000

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -7,11 +7,12 @@ $C "$ a\alias egrep='egrep --colour'" $O
 $C "$ a\alias grep='grep --colour'" $O
 $C "$ a\alias u='cd ..'" $O
 $C "$ a\alias b='cd -'" $O
-$C "$ a\alias l='ls -ahl --color'" $O
+$C "$ a\alias l='ls -ashl --color'" $O
 $C "$ a\alias du1='du -h --max-depth=1'" $O
 $C "$ a\alias sudo='sudo '" $O 
 #sudo sed -i "$ a\alias rmx='rm `ls | grep -v $1`'"
 #sudo sed -i "$ a\alias trim='sed -E 's/\s*$//g' | sed -E 's/^\s*//g''" $O
 $C "$ a\alias http-server='python -m SimpleHTTPServer'" $O
 $C "$ a\alias portwho='netstat -tulpn | grep $1'" $O
+$C "$ a\alias pretty-print='python -mjson.tool'" $O
 source $O
