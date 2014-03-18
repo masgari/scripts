a75d39dfbd1f43e404defaccfc1a301833d33be3
masgari <mammad.asgari@gmail.com>
2014-03-18 16:03:05 +0000

    Update vim-customise.sh

--- vim-customise.sh
+++ vim-customise.sh
@@ -1,1 +1,8 @@
 echo "colorscheme koehler" >> ~/.vimrc
+
+# Adding pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+    
+echo "execute pathogen#infect()"  >> ~/.vimrc
