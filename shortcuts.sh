4802e574d0a0b1c4fe70163c05a9ab5e9eeca195
masgari <mammad.asgari@gmail.com>
2014-03-30 12:23:43 +0100

    Create shortcuts.sh

---
+++ shortcuts.sh
@@ -0,0 +1,6 @@
+#!/bin/bsh -e
+
+# to configure keyboard shortcuts in Ubuntu Desktop
+
+# disable ALT+F1 for working with Intellij IDEA
+gsettings set org.gnome.desktop.wm.keybindings panel-main-menu "[]"
